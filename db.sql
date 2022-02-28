c767d039449212d90bfa247393d0568e25feb291
UB <theurain@naver.com>
2022-02-28 16:58:38 +0900

    2022-02-28 first commit

---
+++ db.sql
@@ -0,0 +1,4 @@
+#DB Creation
+DROP DATABASE IF EXISTS lsite1;
+CREATE DATABASE lsite1;
+use lsite1; 
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    <div class="space-y-4">
+        <div class="t-w-96 t-bg-white t-shadow t-rounded">
+            w-96
+        </div>
+        <div class="t-w-80 bg-blue t-shadow rounded">
+            w-80
+        </div>
+        <div class="w-72 bg-white shadow rounded">
+            w-72
+        </div>
+
+        
+</body>
+</html>
